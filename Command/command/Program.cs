9fc1fd2d91c8a99090b97ae4c4da2d5f509ce0c4
Nogacevschi Nicolae <70882251+brambora1337@users.noreply.github.com>
2022-05-13 15:05:05 +0300

    Add files via upload

---
+++ Command/command/Program.cs
@@ -0,0 +1,26 @@
+﻿using Program;
+
+namespace command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PanasonicTv tv = new PanasonicTv();
+            Speaker speaker = new Speaker();
+
+            Command onCommand = new OnCommand(tv);
+            Command offCommand = new OffCommand(tv);
+
+            Command volumeUpCommand = new VolumeUpCommand(speaker, 10);
+            Command volumeDownCommand = new VolumeDownCommand(speaker);
+            
+
+            UniversalRemote remote = new UniversalRemote(onCommand, offCommand,
+                volumeUpCommand, volumeDownCommand);
+
+            remote.On();
+            remote.VolumeUp();
+        }
+    }
+}
